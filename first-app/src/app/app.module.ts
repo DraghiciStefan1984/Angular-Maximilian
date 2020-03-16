8021851bb217c9e8d9a9db2b2b655b7114a4433d
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-16 07:32:59 +0200

    start

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { WarningAlertComponent } from './components/warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
