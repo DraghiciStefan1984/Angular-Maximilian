ae958c59b694b5e6a879f4f3db54c3a0da4b7c46
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-25 09:35:50 +0200

    added dropdown directive

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DropdownDirective } from './shared/dropdown.directive';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -21,7 +22,8 @@ import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
