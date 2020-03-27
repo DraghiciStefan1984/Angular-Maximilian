16dd48de79eb75ffdca48a58f5471075f311c039
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-03-27 08:28:29 +0200

    services app

---
+++ services-start/src/app/account/account.component.ts
@@ -0,0 +1,21 @@
+import { AccountService } from './../account.service';
+import { LoggingService } from './../logging.service';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private accountService: AccountService) {}
+
+  onSetTo(status: string) 
+  {
+    // this.loggingService.logStatusChanged(status);
+    this.accountService.updateStatus(this.id, status);
+  }
+}
---
+++ services-start/src/app/new-account/new-account.component.ts
@@ -0,0 +1,16 @@
+import { LoggingService } from './../logging.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent 
+{
+  constructor(private loggingService: LoggingService){}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.loggingService.logStatusChanged(accountStatus);
+  }
+}
