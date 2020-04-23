6457439706ddba68f71ff5a808729cd80aa1d2ee
draghicistefan <draghicistefan1984@gmail.com>
2020-04-23 18:34:01 +0300

    added auto logout

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -24,6 +24,8 @@ export class AuthService
     signup_url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
     login_url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
 
+    private tokenExpirationTimer: any;
+
     constructor(private http: HttpClient, private router: Router){}
 
     signup(email: string, password: string)
@@ -52,6 +54,12 @@ export class AuthService
     {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer)
+        {
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer=null;
     }
 
     autoLogin()
@@ -66,7 +74,20 @@ export class AuthService
 
         if(!userData) { return;}
         const loadedUser=new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
-        if(loadedUser.token) { this.user.next(loadedUser); }
+        if(loadedUser.token) 
+        { 
+            this.user.next(loadedUser); 
+            const expirationDuration=new Date(userData._tokenExpirationDate).getTime()-new Date().getTime();
+            this.autoLogout(expirationDuration);
+        }
+    }
+
+    autoLogout(expirationDuration: number)
+    {
+        this.tokenExpirationTimer=setTimeout(()=>
+        {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleError(errorRes: HttpErrorResponse)
@@ -96,6 +117,7 @@ export class AuthService
         const expirationDate=new Date(new Date().getTime()+expiresIn*1000);
         const user=new User(email, userId, token, expirationDate);
         this.user.next(user);
+        this.autoLogout(expiresIn*1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 }
