faace4edcb4fffc057dce16cb51795d496b091de
draghicistefan <draghicistefan1984@gmail.com>
2020-04-23 12:46:49 +0300

    addded autologin

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -54,6 +54,21 @@ export class AuthService
         this.router.navigate(['/auth']);
     }
 
+    autoLogin()
+    {
+        const userData: 
+        {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string
+        }=JSON.parse(localStorage.get('userData'));
+
+        if(!userData) { return;}
+        const loadedUser=new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        if(loadedUser.token) { this.user.next(loadedUser); }
+    }
+
     private handleError(errorRes: HttpErrorResponse)
     {
         let errorMessage='Unknown error...';
@@ -81,5 +96,6 @@ export class AuthService
         const expirationDate=new Date(new Date().getTime()+expiresIn*1000);
         const user=new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 }
