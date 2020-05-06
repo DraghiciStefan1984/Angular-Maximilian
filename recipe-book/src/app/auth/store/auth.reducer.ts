5525c2b77296513fc7a30bc011554547a4226565
draghicistefan <draghicistefan1984@gmail.com>
2020-05-06 11:40:06 +0300

    ngrx auth done

--- recipe-book/src/app/auth/store/auth.reducer.ts
+++ recipe-book/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
     user: User;
@@ -6,6 +7,13 @@ export interface State {
 
 const initialState: State={user: null};
 
-export function authReducer(state: State=initialState, action) {
-    return state;
+export function authReducer(state: State=initialState, action: AuthActions.AuthActions) {
+    switch(action.type){
+        case AuthActions.LOGIN:
+            const user=new User(action.payload.email, action.payload.userId, action.payload.token, action.payload.expirationDate);
+            return {...state, user};
+        case AuthActions.LOGOUT:
+        default:
+            return state;
+    }
 }
